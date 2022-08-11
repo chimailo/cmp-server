29719a76baf8c3ee6ff9e02fb19846a6ed9baba3
chimailo <collins.chima.ilo@gmail.com>
2022-08-11 20:54:48 +0100

    view password security question

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ class Config(object):
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     # sqlalchemy
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
-        'postgres://', 'postgresql://') or \
+        'postgres://', 'postgresql://') or 
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # token
